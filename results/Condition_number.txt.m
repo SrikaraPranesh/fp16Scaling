5478ba3341a0e33f0ac84967341cfce002af7494
Sri <psrikara@gmail.com>
2019-03-18 08:40:31 +0000

    new additions

---
+++ results/Condition_number.txt.m
@@ -0,0 +1,19 @@
+function Condition_number.txt(varargin)
+% CONDITION_NUMBER.TXT Short description of Condition_number.txt.
+%   CONDITION_NUMBER.TXT(VARARGIN) Long description of Condition_number.txt.
+%
+% Example (<a href="matlab:run_example Condition_number.txt">run</a>)
+%
+% See also
+
+%   <author>
+%   Copyright 2019, <institution>
+%
+%   This program is free software: you can redistribute it and/or modify it
+%   under the terms of the GNU General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or (at your
+%   option) any later version. 
+%   See the GNU General Public License for more details. You should have
+%   received a copy of the GNU General Public License along with this
+%   program.  If not, see <http://www.gnu.org/licenses/>.
+
